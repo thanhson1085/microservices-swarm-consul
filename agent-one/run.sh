8fd1b6566b7e2a6c75139dabcc3762cb0a24d4f2
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-12-22 11:22:09 +0000

    typo bugs

--- agent-one/run.sh
+++ agent-one/run.sh
@@ -76,6 +76,5 @@ done
 
 echo Running angular-admin-seed...
 docker pull thanhson1085/angular-admin-seed
-docker run -d \
-    thanhson1085/angular-admin-seed \
-    -p 80
+docker run -d -p 80 \
+    thanhson1085/angular-admin-seed
